06f8362b8ede66ee2155e7a44837b28206a7fc38
wonsuckl <wurijang@naver.com>
2020-10-17 21:26:22 +0900

    add week2 seminar

---
+++ Seminar/SOPT27_week2/SOPT27_week2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SOPT27_week2
+//
+//  Created by 이원석 on 2020/10/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
